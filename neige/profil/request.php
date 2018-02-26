142219c856adba8ff35b3407ae904799c322efd8
G0TA <gotalpha.streaming@gmail.com>
2018-02-26 23:20:30 +0100

    Ajout validation sur request

--- neige/profil/request.php
+++ neige/profil/request.php
@@ -6,12 +6,14 @@ if(isset($_SESSION['id']) AND $_SESSION['id'] > 0) //recupere id de session si i
   {
     $id = $_SESSION['id'];
 		$email = $_SESSION['email'];
+    $validation = $_POST['validation'];
 	  $createdate = date('Y-m-d');
-  	$insertrequest = $bdd->prepare('INSERT INTO request(id,email,createdate)
-    VALUES(?,?,?)') or die(print_r($bdd->errorInfo()));
+  	$insertrequest = $bdd->prepare('INSERT INTO request(id,email,createdate,validation)
+    VALUES(?,?,?,?)') or die(print_r($bdd->errorInfo()));
 		$insertrequest->bindValue(1, $id, PDO::PARAM_INT);
   	$insertrequest->bindValue(2, $email, PDO::PARAM_STR);
   	$insertrequest->bindValue(3, $createdate, PDO::PARAM_STR);
+    $insertrequest->bindValue(4, $validation, PDO::PARAM_STR);
   	$insertrequest->execute();
   	echo "<h6>Demande envoyée !</h6><p>Vous aurez une réponse sous 24h.</p>";
   }
@@ -24,7 +26,7 @@ if(isset($_SESSION['id']) AND $_SESSION['id'] > 0) //recupere id de session si i
     <form method ="post" action ="">
 			<table>
 				<tr>
-					<td></td>
+					<td><input type="hidden" name="validation" value="waiting.."> </td>
 					<td><div class="custom-control custom-checkbox mb-3">
 		      <input type="checkbox" class="custom-control-input" id="customControlValidation1" required>
 		      <label class="custom-control-label" for="customControlValidation1">Accepter les CGU et créer un contrat</label></td>
