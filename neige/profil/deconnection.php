c3b22d6df5c1f20b2f7d5c8ba50e2a96930d0e88
G0TA <gotalpha.streaming@gmail.com>
2018-03-02 09:47:51 +0100

    fonction integrée dans une case

--- neige/profil/deconnection.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); //initialise debut de session
-$_SESSION = array(); //recupere la session en cours
-session_destroy(); //detruit la session
-header("Location: index.php"); //retourne a l'accueil
-?>
