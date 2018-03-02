02be32217912400011e5c2f4215e8d42e04a28c0
KATARS <eckosdu55@gmail.com>
2018-03-02 16:30:27 +0100

    ajout carousel

--- neige/index.php
+++ neige/index.php
@@ -17,24 +17,69 @@ session_start();
     </div>
           <ul class="nav navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" href="./logement/liste_type.php">Louez à la montage</a>
+              <a class="nav-link active" href="./logement/liste_type.php">Catalogue</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" href="index.php?page=1">Espace Personnel</a>
+              <a class="nav-link active" href="index.php?page=1">A propos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" href="index.php?page=2">A propos</a>
+              <a class="nav-link active" href="index.php?page=2">Contact</a>
             </li>
-            <li><a href="#">Page 2</a></li>
+            <li class="nav-item" >
+              <div class="alert alert-info" role="alert">
+                  <strong>-30% jusqu'au 03/06/2018</strong> sur les chalets familiales
+              </div>
+            </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="profil/inscription.php"><span class="glyphicon glyphicon-user"></span> s'inscrire</a></li>
+            <li><a href="profil/connection.php"><span class="glyphicon glyphicon-log-in"></span> se connecter</a></li>
           </ul>
         </div>
       </nav>
+
     <center>
+    </div>
+        <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img class="d-block img-fluid" src="images/montagne1.jpg" alt="montagne1">
+                    <div class="carousel-caption">
+                      <h3>First slide label</h3>
+                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
+                <div class="item">
+                  <img class="d-block img-fluid" src="images/montagne2.jpg" alt="montagne2">
+                  <div class="carousel-caption">
+                      <h1 class="super-heading">Lorem ipsum dolor color</h1>
+                      <p class="super-paragraph">This is a demo for the Bootstrap Carousel Guide.</p>
+                  </div>
+                </div>
+                <div class="item">
+                  <img class="d-block img-fluid" src="images/montagne3.jpg" alt="montagne3">
+                    <div class="carousel-caption">
+                      <h3>Third slide label</h3>
+                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carouselFade" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carouselFade" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
       <h1>Neige & Soleil</h1></br>
+
       <ul class="nav justify-content-center">
         <li class="nav-item">
           <a class="nav-link active" href="./logement/liste_type.php">Louez à la montage</a>
